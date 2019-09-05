3c7dad985760e33fae62a9752dc772b48873adf0
Dulanjali-Liyanage <44173386+Dulanjali-Liyanage@users.noreply.github.com>
2019-09-05 10:14:13 +0530

    insert updating

--- Semester 5/CO322/lab4/text-autocomplete.c
+++ Semester 5/CO322/lab4/text-autocomplete.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 typedef struct trieNode{
 	struct  trieNode *subtrie[26];
-	int isEndOfWord;
+	bool isEndOfWord; // true for ended word and false for not ended word
 }TrieNode;
 
 TrieNode* createNode(){
 	TrieNode* newTrieNode = (TrieNode*)malloc(sizeof(TrieNode));
-	newTrieNode->isEndOfWord = 0;
+	newTrieNode->isEndOfWord = false; // means word has not ended
 
 	int i;
 	for(i = 0;i < 26;i++){
@@ -18,8 +19,14 @@ TrieNode* createNode(){
 	return newTrieNode;
 }
 
-TrieNode* insertWord(TrieNode* root,char* word){
-
+void insertWord(TrieNode* root,char* word){
+	int wordLength = strlen(word);
+	
+	int i;
+	for(i = 0; i < wordLength-1; i++){
+		
+		TrieNode *curr = root
+	}
 }
 
 int printSuggestions(){}
